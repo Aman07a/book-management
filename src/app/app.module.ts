50d4482d29bc9ba3cb7c3b73de5f96f9828ee517
Aman07a <aman16.aa17@gmail.com>
2024-01-16 10:42:01 +0100

    Registering the effect and using Angular Dev Tools to test the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { BookReducer } from './books/book.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './book-list/book-list.component';
 import { AppState } from './app.state';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookEffects } from './books/book.effects';
+import { BookReducer } from './books/book.reducer';
 
 @NgModule({
   declarations: [AppComponent, BookListComponent],
@@ -14,6 +17,8 @@ import { AppState } from './app.state';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot<AppState>({ book: BookReducer }),
+    EffectsModule.forRoot([BookEffects]),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Book } from '../models/book';
 
 @Injectable({
@@ -9,6 +9,9 @@ export class BookService {
   constructor() {}
 
   addBook(book: Book): Observable<Book> {
+    const err = new Error('Error while adding a book');
+    return throwError(() => err);
+
     return of(book);
   }
 }
