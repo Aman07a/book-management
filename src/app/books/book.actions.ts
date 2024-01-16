4ee9287381fdff134aa9d9e1b6793fa06d115d65
Aman07a <aman16.aa17@gmail.com>
2024-01-16 08:46:01 +0100

    Creating the book reducer part 1: Creating a reducer

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -1,8 +1,8 @@
 import { createAction, props } from '@ngrx/store';
 import { Book } from '../models/book';
 
-const AddBlock = createAction('[Book] Add Book', props<Book>());
-const RemoveBlock = createAction(
+export const AddBook = createAction('[Book] Add Book', props<Book>());
+export const RemoveBook = createAction(
   '[Book] Remove Book',
   props<{ bookId: string }>()
 );
